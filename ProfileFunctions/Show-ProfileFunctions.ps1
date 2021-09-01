e443bf0d13295c1516be129a5a6c822155f0f912
Luke Leigh <luke.leigh@gmail.com>
2021-09-01 15:18:15 +0100

    Delete Show-ProfileFunctions.ps1

--- ProfileFunctions/Show-ProfileFunctions.ps1
+++
@@ -1,5 +0,0 @@
-function Show-ProfileFunctions {
-	$Path = Join-Path $PSScriptRoot "/Microsoft.PowerShell_profile.ps1"
-	$functionNames = Get-ContainedCommand $Path -ItemType FunctionDefinition |	Select-Object -ExpandProperty Name
-	$functionNames | Sort-Object
-}
