4cf0cc307d1bc8a178d32ff905fcc6a3eb7f933a
Luke Leigh <luke.leigh@gmail.com>
2021-09-01 14:18:24 +0100

    Update New-Greeting.ps1

--- ProfileFunctions/New-Greeting.ps1
+++ ProfileFunctions/New-Greeting.ps1
@@ -1,11 +1,11 @@
 function New-Greeting {
 	$Today = $(Get-Date)
-	Write-Host "   Day of Week  -"$Today.DayOfWeek " - Today's Date -"$Today.ToShortDateString() "- Current Time -"$Today.ToShortTimeString()
+	Write-Host "Day of Week -"$Today.DayOfWeek " - Today's Date -"$Today.ToShortDateString() "- Current Time -"$Today.ToShortTimeString()
 	Switch ($Today.dayofweek) {
-		Monday { Write-host "   Don't want to work today" }
-		Friday { Write-host "   Almost the weekend" }
-		Saturday { Write-host "   Everyone loves a Saturday ;-)" }
-		Sunday { Write-host "   A good day to rest, or so I hear." }
-		Default { Write-host "   Business as usual." }
+		Monday { Write-Host "Don't want to work today" }
+		Friday { Write-Host "Almost the weekend" }
+		Saturday { Write-Host "Everyone loves a Saturday ;-)" }
+		Sunday { Write-Host "A good day to rest, or so I hear." }
+		Default { Write-Host "Business as usual." }
 	}
 }
