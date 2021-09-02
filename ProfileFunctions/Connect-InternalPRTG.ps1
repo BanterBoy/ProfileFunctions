594525bae25b1f2e4155c3e8bdec8d854a4911ed
Luke Leigh <luke.leigh@gmail.com>
2021-09-02 22:54:13 +0100

    Update Connect-InternalPRTG.ps1

--- ProfileFunctions/Connect-InternalPRTG.ps1
+++ ProfileFunctions/Connect-InternalPRTG.ps1
@@ -1,6 +1,52 @@
 function Connect-InternalPRTG {
-    if(!(Get-PrtgClient))
-        {
-            Connect-PrtgServer -Server "https://csonetmon01.uk.cruk.net" -Credential (Get-Credential)
+
+    <#
+        .SYNOPSIS
+        Connects to the PRTG Monitoring Server.
+        .DESCRIPTION
+        Establishes a secure connection to the PRTG Monitoring Servers API, using the functions from the PrtgAPI module.
+        .PARAMETER url
+        Enter the url for the PRTG Monitoring Server API. This paramter is not mandatory and will default the CSO default PRTG server.
+        .NOTES
+        Name: Connect-InternalPRTG.ps1
+        Author: Luke Leigh
+        DateCreated: 02Sep2021
+
+        .LINK 
+        http://
+        .EXAMPLE
+        Connect-InternalPRTG
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Enter the url for the PRTG Monitoring Server."
+        )]
+        [string]
+        $url = "https://csonetmon01.uk.cruk.net",
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Enter your PRTG credentials."
+        )]
+        [switch]
+        $Credential
+
+    )
+    if ($Credential) {
+        if (!(Get-PrtgClient)) {
+            Connect-PrtgServer -Server $url -Credential $Credential
         }
+    }
+    else {
+        if (!(Get-PrtgClient)) {
+            Connect-PrtgServer -Server $url -Credential (Get-Credential)
+        }
+    }
 }
