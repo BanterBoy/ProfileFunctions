56a7d8146d7976347af8c7ae434d54e0aecee740
Luke Leigh <luke.leigh@gmail.com>
2021-09-29 22:20:51 +0100

    Update Send-MailKitMessage.ps1

--- ProfileFunctions/Send-MailKitMessage.ps1
+++ ProfileFunctions/Send-MailKitMessage.ps1
@@ -1,4 +1,6 @@
-<#
+function Send-MailKitMessage {
+
+    <#
 
     .SYNOPSIS
     Send-MailKitMessage.ps1 - Cmdlet to replace deprecated Send-MailMessage functionality using .Net nuget packages Mailkit & MimeKit.
@@ -42,150 +44,152 @@
 
 #>
 
-[CmdletBinding(DefaultParameterSetName = 'Default',
-    SupportsShouldProcess = $true,
-    HelpUri = 'http://www.microsoft.com/',
-    ConfirmImpact = 'Low')]
-[Alias('smkm')]
-[OutputType([String])]
-Param (
-    # Brief explanation of the parameter and its requirements/function
-    [Parameter(Mandatory = $false,
-        Position = 0,
-        ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [String]
-    $To,
+    [CmdletBinding(DefaultParameterSetName = 'Default',
+        SupportsShouldProcess = $true,
+        HelpUri = 'http://www.microsoft.com/',
+        ConfirmImpact = 'Low')]
+    [Alias('smkm')]
+    [OutputType([String])]
+    Param (
+        # Brief explanation of the parameter and its requirements/function
+        [Parameter(Mandatory = $false,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [String]
+        $To,
 
-    [Parameter( Position = 1,
-        Mandatory = $true,
-        ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [String]
-    $Subject,
+        [Parameter( Position = 1,
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [String]
+        $Subject,
 
-    [Parameter( Position = 2,
-        Mandatory = $true,
-        ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [String]
-    $Body,
+        [Parameter( Position = 2,
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [String]
+        $Body,
 
-    [Parameter( Position = 3,
-        ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [String]
-    $SmtpServer = $PSEmailServer,
+        [Parameter( Position = 3,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [String]
+        $SmtpServer = $PSEmailServer,
 
-    [Parameter( Mandatory = $true,
-        ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [String]
-    $From,
+        [Parameter( Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [String]
+        $From,
 
-    [Parameter( ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [String]
-    $CC,
+        [Parameter( ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [String]
+        $CC,
 
-    [Parameter( ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [String]
-    $BCC,
+        [Parameter( ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [String]
+        $BCC,
 
-    [Parameter( ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [Switch]
-    $BodyAsHtml,
+        [Parameter( ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [Switch]
+        $BodyAsHtml,
         
-    [Parameter( ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [pscredential]
-    $Credential,
+        [Parameter( ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [pscredential]
+        $Credential,
 
-    [Parameter( ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        ValueFromRemainingArguments = $false, 
-        ParameterSetName = 'Default',
-        HelpMessage = "Brief explanation of the parameter and its requirements/function")]
-    [Int32]
-    $Port = 25
+        [Parameter( ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default',
+            HelpMessage = "Brief explanation of the parameter and its requirements/function")]
+        [Int32]
+        $Port = 25
 
-)
+    )
 
-Begin {
-    Add-Type -Path ".\MailKit\MailKit.dll"
-    Add-Type -Path ".\MimeKit\MimeKit.dll"
-}
+    Begin {
+        Add-Type -Path ".\MailKit\MailKit.dll"
+        Add-Type -Path ".\MimeKit\MimeKit.dll"
+    }
 
-Process {
-    $SMTP = New-Object MailKit.Net.Smtp.SmtpClient
-    $Message = New-Object MimeKit.MimeMessage
+    Process {
+        $SMTP = New-Object MailKit.Net.Smtp.SmtpClient
+        $Message = New-Object MimeKit.MimeMessage
   
-    If ($BodyAsHtml) {
-        $TextPart = [MimeKit.TextPart]::new("html")
-    }
-    Else {
-        $TextPart = [MimeKit.TextPart]::new("plain")
-    }
+        If ($BodyAsHtml) {
+            $TextPart = [MimeKit.TextPart]::new("html")
+        }
+        Else {
+            $TextPart = [MimeKit.TextPart]::new("plain")
+        }
       
-    $TextPart.Text = $Body
+        $TextPart.Text = $Body
   
-    $Message.From.Add($From)
-    $Message.To.Add($To)
+        $Message.From.Add($From)
+        $Message.To.Add($To)
       
-    If ($CC) {
-        $Message.CC.Add($CC)
-    }
+        If ($CC) {
+            $Message.CC.Add($CC)
+        }
       
-    If ($BCC) {
-        $Message.BCC.Add($BCC)
-    }
+        If ($BCC) {
+            $Message.BCC.Add($BCC)
+        }
   
-    $Message.Subject = $Subject
-    $Message.Body = $TextPart
+        $Message.Subject = $Subject
+        $Message.Body = $TextPart
   
-    $SMTP.Connect($SmtpServer, $Port, $False)
+        $SMTP.Connect($SmtpServer, $Port, $False)
   
-    If ($Credential) {
-        $SMTP.Authenticate($Credential.UserName, $Credential.GetNetworkCredential().Password)
-    }
+        If ($Credential) {
+            $SMTP.Authenticate($Credential.UserName, $Credential.GetNetworkCredential().Password)
+        }
   
-    If ($PSCmdlet.ShouldProcess('Send the mail message via MailKit.')) {
-        $SMTP.Send($Message)
-    }
+        If ($PSCmdlet.ShouldProcess('Send the mail message via MailKit.')) {
+            $SMTP.Send($Message)
+        }
     
-}
+    }
 
-end {
-    $SMTP.Disconnect($true)
-    $SMTP.Dispose()
+    end {
+        $SMTP.Disconnect($true)
+        $SMTP.Dispose()
+    }
+
 }
 
