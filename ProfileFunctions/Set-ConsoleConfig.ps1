a533b139f01b62095b7e22d6e992bc6d0af39b3d
Luke Leigh <luke.leigh@gmail.com>
2021-12-29 09:15:16 +0000

    Update Set-ConsoleConfig.ps1

--- ProfileFunctions/Set-ConsoleConfig.ps1
+++ ProfileFunctions/Set-ConsoleConfig.ps1
@@ -1,9 +1,7 @@
 function Set-ConsoleConfig {
     param (
         [int]$WindowHeight,
-        [int]$WindowWidth,
-        [int]$BufferHeight=9001
+        [int]$WindowWidth
     )
     [System.Console]::SetWindowSize($WindowWidth, $WindowHeight)
-    [System.Console]::SetBufferSize($WindowWidth, $BufferHeight)
 }
