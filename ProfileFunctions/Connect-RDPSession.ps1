198ba33a3280a4467dee7ec4608c5b1cea5ce94b
Luke Leigh <luke.leigh@gmail.com>
2022-01-26 02:10:09 +0000

    Update Connect-RDPSession.ps1

--- ProfileFunctions/Connect-RDPSession.ps1
+++ ProfileFunctions/Connect-RDPSession.ps1
@@ -1,4 +1,32 @@
 function Connect-RDPSession {
+
+    <#
+        .SYNOPSIS
+            Connect-RDPSession
+        .DESCRIPTION
+            Connect-RDPSession - Spawn MSTSC and launch an RDP session to a remote computer.
+        .PARAMETER
+            ComputerName
+            The name of the remote computer to connect to.
+        .EXAMPLE
+            Connect-RDPSession -ComputerName COMPUTERNAME
+            Starts an RDP session to COMPUTERNAME
+        .OUTPUTS
+            System.String. Connect-RDPSession
+        .NOTES
+            Author:     Luke Leigh
+            Website:    https://scripts.lukeleigh.com/
+            LinkedIn:   https://www.linkedin.com/in/lukeleigh/
+            GitHub:     https://github.com/BanterBoy/
+            GitHubGist: https://gist.github.com/BanterBoy
+        .INPUTS
+            ComputerName - You can pipe objects to this perameters.
+        .LINK
+            https://scripts.lukeleigh.com
+            Get-Date
+            Write-Output
+    #>
+    
     [CmdletBinding(DefaultParameterSetName = 'Default',
         PositionalBinding = $true)]
     [OutputType([string], ParameterSetName = 'Default')]
