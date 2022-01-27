8658d65254d9bc83c815e094b1bbe536cf829326
Luke Leigh <luke.leigh@gmail.com>
2022-01-27 16:21:04 +0000

    Update Get-ComputerIP.ps1

--- ProfileFunctions/Get-ComputerIP.ps1
+++ ProfileFunctions/Get-ComputerIP.ps1
@@ -1,27 +1,5 @@
 function Get-ComputerIP {
 
-    <#
-    .SYNOPSIS
-    Tests computer for connection and lists status with IP address.
-    .DESCRIPTION
-    Tests computer for connection and lists status with IP address.
-    .PARAMETER server
-    Name of server to test connection to.
-    .PARAMETER file
-    Name of host file to test connection to.
-    .PARAMETER credential
-    Allows the use of alternate credentials, if required.
-    .NOTES
-    Name: Get-ComputerIP.ps1
-    Author: Boe Prox
-    DateCreated: 05Aug2010
-
-    .LINK 
-    http://
-    .EXAMPLE
-    Get-ComputerIP
-
-    #>
     [cmdletbinding(
         SupportsShouldProcess = $True,
         DefaultParameterSetName = 'computer',
@@ -53,23 +31,25 @@ function Get-ComputerIP {
     }
     $report = @()
     ForEach ($Computer in $Computers) {
-        Try {
-            $tempreport = New-Object PSObject
-            If ($credential) {
-                $IP = ((Test-Connection -ErrorAction Stop -Count 1 -ComputerName $Computer -Credential $cred).IPV4Address).IPAddresstoString
+        if ($PSCmdlet.ShouldProcess("Target", "Operation")) {
+            Try {
+                $tempreport = New-Object PSObject
+                If ($credential) {
+                    $IP = ((Test-Connection -ErrorAction Stop -Count 1 -ComputerName $Computer -Credential $cred).IPV4Address).IPAddresstoString
+                }
+                Else {
+                    $IP = ((Test-Connection -ErrorAction Stop -Count 1 -ComputerName $Computer).IPV4Address).IPAddresstoString
+                }
+                $tempreport | Add-Member NoteProperty Computer $Computer
+                $tempreport | Add-Member NoteProperty Status “Up”
+                $tempreport | Add-Member NoteProperty IP $IP
+                $report += $tempreport
             }
-            Else {
-                $IP = ((Test-Connection -ErrorAction Stop -Count 1 -ComputerName $Computer).IPV4Address).IPAddresstoString
+            Catch {
+                $tempreport = New-Object PSObject
+                $tempreport | Add-Member NoteProperty Computer $Computer
+                $tempreport | Add-Member NoteProperty Status “Down”
             }
-            $tempreport | Add-Member NoteProperty Computer $Computer
-            $tempreport | Add-Member NoteProperty Status “Up”
-            $tempreport | Add-Member NoteProperty IP $IP
-            $report += $tempreport
-        }
-        Catch {
-            $tempreport = New-Object PSObject
-            $tempreport | Add-Member NoteProperty Computer $Computer
-            $tempreport | Add-Member NoteProperty Status “Down”
         }
     }
     $report
