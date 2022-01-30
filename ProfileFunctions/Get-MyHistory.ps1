f82f0723e80b264f3e2b8da0ca44ac47437b810f
Luke Leigh <luke.leigh@gmail.com>
2022-01-30 20:07:56 +0000

    Update Get-MyHistory.ps1

--- ProfileFunctions/Get-MyHistory.ps1
+++ ProfileFunctions/Get-MyHistory.ps1
@@ -1,10 +1,39 @@
-function Get-MyHistory {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [int]
-        $Quantity
-    )
-    Get-History | Select-Object -Property CommandLine -Last $Quantity | Format-Table -AutoSize -Wrap
-
+Function Get-MyHistory
+{
+<#
+	.SYNOPSIS
+		Get-MyHistory will recall the previous commands entered into the console in a format that is easy to copy and paste.
+	
+	.DESCRIPTION
+		Get-MyHistory will recall the previous commands entered into the console in a format that is easy to copy and paste.
+	
+	.PARAMETER Quantity
+		Enter a value between 1 and 9999 to recall the number of hitorical commands.
+	
+	.EXAMPLE
+				PS C:\> Get-MyHistory
+	
+	.OUTPUTS
+		string
+	
+	.NOTES
+		Additional information about the function.
+#>
+	
+	[CmdletBinding(DefaultParameterSetName = 'Default',
+				   PositionalBinding = $true)]
+	[OutputType([string], ParameterSetName = 'Default')]
+	Param
+	(
+		[Parameter(Mandatory = $false,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   Position = 0)]
+		[ValidateSet('1', '9999')]
+		[Alias('q')]
+		[int32]
+		$Quantity = 1
+	)
+	
+	Get-History | Select-Object -Property CommandLine -Last $Quantity
 }
