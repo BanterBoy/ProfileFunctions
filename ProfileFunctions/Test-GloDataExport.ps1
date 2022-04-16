d2311663500f606fef32b39975792575cb9dce8d
Luke Leigh <luke.leigh@gmail.com>
2022-04-16 04:06:49 +0100

    Delete Test-GloDataExport.ps1

--- ProfileFunctions/Test-GloDataExport.ps1
+++
@@ -1,5 +0,0 @@
-function Test-GloDataExport {
-    $GloExport = "CarpetrightGlo-" + [DateTime]::ParseExact( (Get-Date).ToShortDateString() , "dd/MM/yyyy", $null).ToString("yyyy-MM-dd") + "-import.csv"
-    $Filepath = "C:\GitRepos\Carpetright\NewUserProcess\HRData\postreleasechanges\"
-    Export-GloDataExtract -Path $Filepath -Filename $GloExport
-}     
