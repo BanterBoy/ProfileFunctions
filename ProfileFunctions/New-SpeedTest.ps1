8893d0e5088f63e63a3dcf442d878f67536a9573
Luke Leigh <luke.leigh@gmail.com>
2022-04-22 02:09:00 +0100

    Update New-SpeedTest.ps1

--- ProfileFunctions/New-SpeedTest.ps1
+++ ProfileFunctions/New-SpeedTest.ps1
@@ -1,4 +1,81 @@
 function New-SpeedTest {
+
+    <#
+        Options
+        -h, --help
+        Print usage information
+
+        -v Logging verbosity, specify multiple times for higher verbosity (e.g. -vvv)
+
+        -V, --version Print version number
+
+        -L, --servers List nearest servers
+
+        --selection-details Show server selection details
+
+        -s id, --server-id=id
+        Specify a server from the server list using its id
+
+        -o hostname, --host=hostname
+        Specify a server from the server list using its hostname
+
+        -f format_type --format=format_type
+        Output format (default = human-readable) Note: Machine readable formats (csv, tsv, json, jsonl, json-pretty) use bytes as the unit of measure with max precision.
+
+        format_type values are as follows:
+
+        human-readable human readable output
+        csv comma separated values
+        tsv tab separated values
+        json javascript object notation (compact)
+        jsonl javascript object notation (lines)
+        json-pretty javascript object notation (pretty)
+        --progress-update-interval=interval Progress update interval (100-1000 milliseconds)
+
+        --output-header Show output header for CSV and TSV formats
+
+        -u* unit_of_measure***, --unit*** unit_of_measure* Output unit for displaying speeds (Note: this is only applicable for ‘human-readable’ output format and the default unit is Mbps)
+
+        bps bits per second (decimal prefix)
+        kbps kilobits per second (decimal prefix)
+        Mbps megabits per second (decimal prefix)
+        Gbps gigabits per second (decimal prefix)
+        kibps kilobits per second (binary prefix)
+        Mibps megabits per second (binary prefix)
+        Gibps gigabits per second (binary prefix)
+        B/s bytes per second
+        kB/s kilobytes per second
+        MB/s megabytes per second
+        GiB/s gigabytes per second
+        auto-binary-bytes automatic in binary bytes
+        auto-decimal-bytes automatic in decimal bytes
+        auto-binary-bytes automatic in binary bits
+        auto-binary-bytes automatic in decimal bits
+        -a
+        Shortcut for [-u auto-decimal-bits]
+
+        -A
+        Shortcut for [-u auto-decimal-bytes]
+
+        -b
+        Shortcut for [-u auto-binary-bits]
+
+        -B
+        Shortcut for [-u auto-binary-bytes]
+
+        -P decimal_places --precision=decimal_places
+        Number of decimal_places to use (default = 2, valid = 0-8)
+
+        -p yes|no --progress=yes|no
+        Enable or disable progress bar (default = yes when interactive)
+
+        -I interface --interface=interface Attempt to bind to the specified interface when connecting to servers
+
+        -i ip_address --ip=ip_address Attempt to bind to the specified IP address when connecting to servers
+
+        --ca-certificate=path Path to CA Certificate bundle, see note below.
+    #>
+
     $Path = ([Environment]::GetFolderPath("Desktop")) + "\SpeedTestResults"
     $Speedtest = & $PSScriptRoot\speedtest.exe --format=json --accept-license --accept-gdpr
     $Speedtest | Out-File ("$Path\Result-" + ([datetime]::Today).ToShortDateString().replace("/", "-") + "-" + ([datetime]::Now).ToLongTimeString().replace(":", "-") + ".txt") -Force
