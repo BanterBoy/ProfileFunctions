fbcfed9553c0797b3559ac01b1db55908940f8ab
Luke Leigh <luke.leigh@gmail.com>
2022-05-02 04:56:40 +0100

    Create ExampleParamsInScript.ps1

---
+++ ExampleParamsInScript.ps1
@@ -0,0 +1,36 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+	[Parameter(ParameterSetName = 'Default',
+		Mandatory = $false,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true,
+		HelpMessage = 'Enter computer name or pipe input'
+	)]
+	[Alias('cn')]
+	[string[]]$ComputerName = $env:COMPUTERNAME,
+	[Parameter(ParameterSetName = 'Default',
+		Mandatory = $false,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true,
+		HelpMessage = 'Enter computer name or pipe input'
+	)]
+	[Alias('cred')]
+	[ValidateNotNull()]
+	[System.Management.Automation.PSCredential]
+	[System.Management.Automation.Credential()]
+	$Credential,
+	[Parameter(ParameterSetName = 'Default',
+		Mandatory = $false,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true,
+		HelpMessage = 'Enter computer name or pipe input'
+	)]
+	[Alias('sam')]
+	[string[]]$SamAccountName
+)
+Process {
+	if ($PSCmdlet.ShouldProcess("$ComputerName / $SamAccountName", "ExampleParamsInScript")) {
+		# Script content goes here
+		
+	}
+}
