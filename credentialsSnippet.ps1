7c9f3f756bd5301345807c08f5bb90ddfda2a137
Luke Leigh <luke.leigh@gmail.com>
2022-05-02 04:56:30 +0100

    Create credentialsSnippet.ps1

---
+++ credentialsSnippet.ps1
@@ -0,0 +1,19 @@
+[Parameter(ParameterSetName = 'Default',
+	Mandatory = $false,
+	ValueFromPipeline = $true,
+	ValueFromPipelineByPropertyName = $true,
+	HelpMessage = 'Enter computer name or pipe input'
+)]
+[Alias('cn')]
+[string[]]$ComputerName = $env:COMPUTERNAME,
+[Parameter(ParameterSetName = 'Default',
+	Mandatory = $false,
+	ValueFromPipeline = $true,
+	ValueFromPipelineByPropertyName = $true,
+	HelpMessage = 'Enter computer name or pipe input'
+)]
+[Alias('cred')]
+[ValidateNotNull()]
+[System.Management.Automation.PSCredential]
+[System.Management.Automation.Credential()]
+$Credential
