373a5483b4a1450cb2d28c274ef6f1cd3036a0ca
Luke Leigh <luke.leigh@gmail.com>
2022-05-02 04:56:33 +0100

    Create CredParamsInScript.ps1

---
+++ CredParamsInScript.ps1
@@ -0,0 +1,21 @@
+[Parameter(ParameterSetName = 'Default',
+	Mandatory = $false,
+	ValueFromPipeline = $true,
+	ValueFromPipelineByPropertyName = $true,
+	HelpMessage = 'Enter computer name or pipe input'
+)]
+[Alias('cn')]
+[string[]]$ComputerName = $env:COMPUTERNAME,
+[Parameter(ParameterSetName = 'Default',
+	Mandatory = $false,
+	ValueFromPipeline = $true,
+	ValueFromPipelineByPropertyName = $true,
+	HelpMessage = 'Enter computer name or pipe input'
+)]
+[Alias('cred')]
+[ValidateNotNull()]
+[System.Management.Automation.PSCredential]
+[System.Management.Automation.Credential()]
+$Credential
+
+Add-ADGroupMember -Credential $Credential -WhatIf
