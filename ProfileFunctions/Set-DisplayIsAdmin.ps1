3d8f50efa3cba6646978a555cbcb0b58f2771421
Luke Leigh <luke.leigh@gmail.com>
2022-05-09 07:58:42 +0100

    Update Set-DisplayIsAdmin.ps1

--- ProfileFunctions/Set-DisplayIsAdmin.ps1
+++ ProfileFunctions/Set-DisplayIsAdmin.ps1
@@ -1,23 +1,11 @@
 function Set-DisplayIsAdmin {
 
     if ((Test-IsAdmin) -eq $true) {
-        if ((Get-Host).Version.Major -eq '5') {
-			$Username = (Get-WMIObject -ClassName Win32_ComputerSystem).Username
-            $host.UI.RawUI.WindowTitle = "$($Username) - Admin Privileges"
-        }
-        else {
-			$Username = (Get-CimInstance -ClassName Win32_ComputerSystem).Username
-            $host.UI.RawUI.WindowTitle = "$($Username) - Admin Privileges"
-        }
+		$Username = whoami.exe /upn
+		$host.UI.RawUI.WindowTitle = "$($Username) - Admin Privileges"
     }	
     else {
-        if ((Get-Host).Version.Major -eq '5') {
-			$Username = (Get-WMIObject -ClassName Win32_ComputerSystem).Username
-            $host.UI.RawUI.WindowTitle = "$($Username) - User Privileges"
-        }
-        else {
-            $Username = (Get-CimInstance -ClassName Win32_ComputerSystem).Username
-            $host.UI.RawUI.WindowTitle = "$($Username) - User Privileges"
-        }
+		$Username = whoami.exe /upn
+		$host.UI.RawUI.WindowTitle = "$($Username) - User Privileges"
     }	
 }
