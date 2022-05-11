17ac82dce163d8b50fefff9245689486c7a1827f
Luke Leigh <luke.leigh@gmail.com>
2022-05-11 15:51:18 +0100

    Create OhMyPoshInstall.ps1

---
+++ OhMyPoshInstall.ps1
@@ -0,0 +1,10 @@
+# Install Command
+winget install oh-my-posh
+or
+winget install oh-my-posh --accept-source-agreements --accept-package-agreements
+
+# Profile Changes
+oh-my-posh init pwsh | Invoke-Expression
+
+
+# Recommended font - Meslo LGM NF
