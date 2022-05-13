dfbb3f57141b06f5d3c4634290603a8799ad6ac8
Luke Leigh <luke.leigh@gmail.com>
2022-05-13 08:25:36 +0100

    Update TransmissionFunctions.ps1

--- TransmissionFunctions.ps1
+++ TransmissionFunctions.ps1
@@ -14,7 +14,7 @@ Set-TransmissionDefaultSettings
 function Set-TransmissionDefaultSettings {
     $path = "/share/Download/transmission"
 
-	Set-TransmissionSession -SeedRatioLimit 0 -SeedRatioLimited $True -IncompleteDirectory "$path/incomplete" -DownloadDirectory "$path/completed" -DownloadQueueSize 15
+    Set-TransmissionSession -SeedRatioLimit 0 -SeedRatioLimited $True -IncompleteDirectory "$path/incomplete" -DownloadDirectory "$path/completed" -DownloadQueueSize 15
 }
 
 <#
@@ -40,3 +40,50 @@ function Test-TransmissionSettings {
         Write-Warning -Message "Failed to get transmission settings, NAS may be offline..."
     }
 }
+
+$properties = @{
+    AlternativeSpeedDown        = 1024
+    AlternativeSpeedEnabled     = true
+    AlternativeSpeedTimeBegin   = 480
+    AlternativeSpeedTimeDay     = 127
+    AlternativeSpeedTimeEnabled = true
+    AlternativeSpeedTimeEnd     = 60
+    AlternativeSpeedUp          = 128
+    BlockListEnabled            = true
+    BlockListUrl                = "http://john.bitsurge.net/public/biglist.p2p.gz"
+    CacheSizeMb                 = 8
+    DhtEnabled                  = true
+    DownloadDirectory           = "/share/Public"
+    DownloadQueueEnabled        = true
+    DownloadQueueSize           = 1
+    Encryption                  = "required"
+    IdleSeedingLimit            = 0
+    IdleSeedingLimitEnabled     = true
+    IncompleteDirectory         = "/share/Public"
+    IncompleteDirectoryEnabled  = true
+    LpdEnabled                  = false
+    PeerLimitGlobal             = 500
+    PeerLimitPerTorrent         = 250
+    PeerPort                    = 51413
+    PeerPortRandomOnStart       = false
+    PexEnabled                  = true
+    PortForwardingEnabled       = true
+    QueueStalledEnabled         = true
+    QueueStalledMinutes         = 0
+    RenamePartialFiles          = true
+    ScriptTorrentDoneEnabled    = false
+    ScriptTorrentDoneFilename   = ""
+    SeedQueueEnabled            = true
+    SeedQueueSize               = 0
+    SeedRatioLimit              = 0
+    SeedRatioLimited            = true
+    SpeedLimitDown              = 10240
+    SpeedLimitDownEnabled       = true
+    SpeedLimitUp                = 512
+    SpeedLimitUpEnabled         = true
+    StartAddedTorrents          = false
+    TrashOriginalTorrentFiles   = false
+    UtpEnabled                  = true
+}
+
+Set-TransmissionDefaultSettings @properties
