9edf94e4d8ce503b478f3a6a80cf1fb3300f53e7
Luke Leigh <luke.leigh@gmail.com>
2022-05-13 14:55:58 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,26 +2,25 @@
 # Generic Profile Commands
 #--------------------
 Get-ChildItem C:\GitRepos\ProfileFunctions\ProfileFunctions\*.ps1 | ForEach-Object {. $_ }
+# oh-my-posh init pwsh | Invoke-Expression
+# oh-my-posh print primary --config C:\GitRepos\ProfileFunctions\BanterBoyOhMyPoshTheme.json --shell uni
+oh-my-posh init pwsh --config C:\GitRepos\ProfileFunctions\BanterBoyOhMyPoshConfig.json| Invoke-Expression
 
 #--------------------
 # Set-ExecutionPolicy
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
-
-#--------------------
-# Display running as Administrator in WindowTitle
-Set-DisplayIsAdmin
-
-function Connect-Office365Services {
-	& (Join-Path ($PROFILE).TrimEnd('Microsoft.PowerShell_profile.ps1') "\Connect-Office365Services.ps1")
-}
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
 
 # basic greeting function, contents to be added to current function
-Write-Host "Type Get-ProfileFunctions to see the available functions"
-Write-Host ""
+Write-Output "Type Get-ProfileFunctions to see the available functions"
 
 #--------------------
 # Configure PowerShell Console Window Size/Preferences
-Set-ConsoleConfig -WindowHeight 45 -WindowWidth 180
+Set-ConsoleConfig -WindowHeight 45 -WindowWidth 180 | Out-Null
+
+#--------------------
+# PSDrives
+New-PSDrive -Name GitRepos -PSProvider FileSystem -Root C:\GitRepos\ -Description "GitHub Repositories" | Out-Null
+New-PSDrive -Name Sysint -PSProvider FileSystem -Root "$env:OneDrive\Software\SysinternalsSuite" -Description "Sysinternals Suite Software" | Out-Null
 
 #--------------------
 # Aliases
@@ -29,25 +28,8 @@ New-Alias -Name 'Notepad++' -Value 'C:\Program Files\Notepad++\notepad++.exe' -D
 
 #--------------------
 # Profile Starts here!
-# $DaysLeft = (New-TimeSpan -Start (Get-Date) -End ((Get-Date).AddMonths("1").Date)).Days
-# $properties = [ordered]@{
-# 	PayDay = (Get-PayDay).DayofWeek
-# 	PayDate = (Get-PayDay).LongDate
-# 	DaysLeft = $DaysLeft
-# }
-# if ($DaysLeft -gt ($DaysLeft / 3 * 2) ) {
-# 	Write-Host "Next PayDay Date   : $($properties.PayDay) $($properties.PayDate)" -ForegroundColor Blue -NoNewline:$false
-# 	Write-Host "Days until Pay Day : $($DaysLeft) Days Left" -ForegroundColor Blue
-# }
-# elseif ($DaysLeft -lt ($DaysLeft / 2) ) {
-# 	Write-Host "Next PayDay Date   : $($properties.PayDay) $($properties.PayDate)" -ForegroundColor Gray -NoNewline:$false
-# 	Write-Host "Days until Pay Day : $($DaysLeft) Days Left" -ForegroundColor Gray
-# }
-# else { 
-# 	Write-Host "Next PayDay Date   : $($properties.PayDay) $($properties.PayDate)" -ForegroundColor Green -NoNewline:$false
-# 	Write-Host "Days until Pay Day : $($DaysLeft) Days Left" -ForegroundColor Green
-# }
-Write-Host ""
+Write-Output ""
 Show-IsAdminOrNot
-Write-Host ""
+Write-Output ""
 New-Greeting
+Set-Location -Path GitRepos:\
