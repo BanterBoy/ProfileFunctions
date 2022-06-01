5774f83add311d05a6ae962dc1635a24490a568e
Luke Leigh <luke.leigh@gmail.com>
2022-06-01 01:36:37 +0100

    Create Get-Lines.ps1

---
+++ ProfileFunctions/Get-Lines.ps1
@@ -0,0 +1,49 @@
+function Get-Lines {
+    <#
+    .SYNOPSIS
+    Counts the number of lines in a file.
+    #>
+    [cmdletbinding(DefaultParameterSetName = 'Path')]
+    param(
+        [parameter(
+            Mandatory,
+            ParameterSetName = 'Path',
+            Position = 0,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName
+        )]
+        [ValidateNotNullOrEmpty()]
+        [SupportsWildcards()]
+        [string[]]$Path,
+
+        [parameter(
+            Mandatory,
+            ParameterSetName = 'LiteralPath',
+            Position = 0,
+            ValueFromPipelineByPropertyName
+        )]
+        [ValidateNotNullOrEmpty()]
+        [Alias('PSPath')]
+        [string[]]$LiteralPath
+    )
+
+    process {
+        # Resolve path(s)
+        if ($PSCmdlet.ParameterSetName -eq 'Path') {
+            $resolvedPaths = Resolve-Path -Path $Path | Select-Object -ExpandProperty Path
+        }
+        elseif ($PSCmdlet.ParameterSetName -eq 'LiteralPath') {
+            $resolvedPaths = Resolve-Path -LiteralPath $LiteralPath | Select-Object -ExpandProperty Path
+        }
+
+        # Process each item in resolved paths
+        foreach ($item in $resolvedPaths) {
+            $fileItem = Get-Item -LiteralPath $item
+            $content = $fileItem | Get-Content
+            [pscustomobject]@{
+                Path  = $fileItem.Name
+                Lines = $content.Count
+            }
+        }
+    }
+}
