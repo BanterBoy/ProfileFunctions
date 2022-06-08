debcb68eb41fb95281531ef04a2d3e82d38e8a70
Luke Leigh <luke.leigh@gmail.com>
2022-06-08 20:37:12 +0100

    Update Remove-JekyllBlogServer.ps1

--- ProfileFunctions/Remove-JekyllBlogServer.ps1
+++ ProfileFunctions/Remove-JekyllBlogServer.ps1
@@ -6,23 +6,25 @@ function Remove-JekyllBlogServer {
 	if ($decision -eq 0) {
 		$directoryPath = Select-FolderLocation
 		if (![string]::IsNullOrEmpty($directoryPath)) {
-			Write-Host "You selected the directory: $directoryPath"
+			Write-Output "You selected the directory: $directoryPath"
 		}
 		else {
 			"You did not select a directory."
 		}
-		Write-Host 'Cleaning Environment - Removing Images'
+		Write-Output 'Cleaning Environment - Removing Images'
 		$images = docker images jekyll/jekyll:latest -q
 		foreach ($image in $images) {
 			docker image rm $image -f
-			docker image rm $(docker ps -a -f status=exited -q)
+			docker rm $(docker ps -qf 'status=exited')
+			docker rmi $(docker images -qf 'dangling=true')
+			docker volume rm $(docker volume ls -qf 'dangling=true')
 		}
 
-		$vendor = ($directoryPath + "vendor")
-		$site = ($directoryPath + "_site")
-		$gemfile = ($directoryPath + "gemfile.lock")
-		$jekyllmetadata = ($directoryPath + ".jekyll-metadata")
-		$jekyllcache = ($directoryPath + ".jekyll-cache")
+		$vendor = ($directoryPath + "\vendor")
+		$site = ($directoryPath + "\_site")
+		$gemfile = ($directoryPath + "\gemfile.lock")
+		$jekyllmetadata = ($directoryPath + "\.jekyll-metadata")
+		$jekyllcache = ($directoryPath + "\.jekyll-cache")
 
 		$vendorPath = Test-Path -Path $vendor
 		$sitePath = Test-Path -Path $site
@@ -30,7 +32,7 @@ function Remove-JekyllBlogServer {
 		$jekyllmetadataPath = Test-Path -Path $jekyllmetadata
 		$jekyllcachePath = Test-Path -Path $jekyllcache
 
-		if ( $vendorPath = $true ) {
+		if ( $vendorPath ) {
 			Write-Warning -Message 'Cleaning Environment - Removing Vendor Bundle'
 			try {
 				Remove-Item -Path $vendor -Recurse -Force -Recurse -ErrorAction Stop
@@ -40,7 +42,7 @@ function Remove-JekyllBlogServer {
 				Write-Verbose -Message 'Vendor Bundle does not exist.' -Verbose
 			}
 		}
-		if ( $sitePath = $true ) {
+		if ( $sitePath ) {
 			Write-Warning -Message 'Cleaning Environment - Removing _site Folder'
 			try {
 				Remove-Item -Path $site -Recurse -Force -Recurse -ErrorAction Stop
@@ -50,7 +52,7 @@ function Remove-JekyllBlogServer {
 				Write-Verbose -Message '_site folder does not exist.' -Verbose
 			}
 		}
-		if ( $gemfilePath = $true ) {
+		if ( $gemfilePath  ) {
 			Write-Warning -Message 'Cleaning Environment - Removing Gemfile.lock File'
 			try {
 				Remove-Item -Path $gemfile -Force -Recurse -ErrorAction Stop
@@ -60,7 +62,7 @@ function Remove-JekyllBlogServer {
 				Write-Verbose -Message 'gemfile.lock does not exist.' -Verbose
 			}
 		}
-		if ( $jekyllmetadataPath = $true ) {
+		if ( $jekyllmetadataPath ) {
 			Write-Warning -Message 'Cleaning Environment - Removing .jekyll-metadata File'
 			try {
 				Remove-Item -Path $jekyllmetadata -Force -Recurse -ErrorAction Stop
@@ -70,7 +72,7 @@ function Remove-JekyllBlogServer {
 				Write-Verbose -Message '.jekyll-metadata does not exist.' -Verbose
 			}
 		}
-		if ( $jekyllcachePath = $true ) {
+		if ( $jekyllcachePath ) {
 			Write-Warning -Message 'Cleaning Environment - Removing .jekyll-cache File'
 			try {
 				Remove-Item -Path $jekyllcache -Force -Recurse -ErrorAction Stop
