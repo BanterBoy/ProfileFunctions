e5066e3adcd7e3792106806c38c2bb1cbb4c0323
Luke Leigh <luke.leigh@gmail.com>
2022-08-22 15:45:12 +0100

    Create ServerSelection.ps1

---
+++ ServerSelection.ps1
@@ -0,0 +1,21 @@
+$Results = Get-Content -Path C:\Temp\Result-22-08-2022-13-38-20.json | ConvertFrom-Json
+$Servers = $Results.serverSelection.servers
+foreach ($Server in $Servers) {
+    Try {
+        $properties = [ordered]@{
+            ID       = $Server.server.id
+            Host     = $Server.server.host
+            Port     = $Server.server.port
+            Name     = $Server.server.name
+            Location = $Server.server.location
+            Country  = $Server.server.country
+        }
+    }
+    Catch {
+        Write-Error $_
+    }
+    Finally {
+        $obj = New-Object -TypeName PSObject -Property $properties
+        Write-Output $obj
+    }
+}
