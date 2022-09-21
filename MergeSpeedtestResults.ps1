7051b820760a465b6f3a23f6e02a99544c038831
Luke Leigh <luke.leigh@gmail.com>
2022-09-21 19:31:42 +0100

    Update MergeSpeedtestResults.ps1

--- MergeSpeedtestResults.ps1
+++ MergeSpeedtestResults.ps1
@@ -1,15 +1,4 @@
-$totalDetails = @()
- 
-$Ind_Details = 
-$UK_Details = 
- 
-$totalDetails += ConvertFrom-Json  $Ind_Details
-$totalDetails += ConvertFrom-Json  $UK_Details
- 
-$totalDetails = $totalDetails | ConvertTo-Json
-$totalDetails
 
+$Files = Get-ChildItem -Path "$env:OneDriveCommercial\Documents\SpeedTestResults\" -Filter *.json
 
-
-Get-ChildItem -Path "C:\Users\Luke\OneDrive - leighzhao\Documents\SpeedTestResults\" -Filter *.json | Select-Object -ExpandProperty FullName | ConvertFrom-Json | ConvertTo-Json | Out-File -Path .\merged.json -Encoding UTF8
-Get-ChildItem -Path "C:\Users\Luke\OneDrive - leighzhao\Documents\SpeedTestResults\" -Filter *.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv  -NoTypeInformation | Out-File -Path .\merged.csv -Encoding UTF8
+$Files.FullName | ForEach-Object -Process { Get-Content -Path $_ | ConvertFrom-Json } | ConvertTo-Json -Depth 5 | Out-File -Path "$env:OneDriveCommercial\Documents\SpeedTestResults\merged.json" -Encoding UTF8 -Append
