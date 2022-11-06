1124fd482416651e439b760012ba2a8c0ac1fb9e
Luke Leigh <luke.leigh@gmail.com>
2022-11-06 23:39:54 +0000

    Update MergeSpeedtestResults.ps1

--- MergeSpeedtestResults.ps1
+++ MergeSpeedtestResults.ps1
@@ -1,4 +1,7 @@
+# $Files = Get-ChildItem -Path "$env:OneDriveCommercial\Documents\SpeedTestResults\" -Filter *.json
+# $Files.FullName | ForEach-Object -Process { Get-Content -Path $_ | ConvertFrom-Json } | ConvertTo-Json -Depth 5 | Out-File -Path "$env:OneDriveCommercial\Documents\SpeedTestResults\merged.json" -Encoding UTF8 -Append
 
-$Files = Get-ChildItem -Path "$env:OneDriveCommercial\Documents\SpeedTestResults\" -Filter *.json
 
-$Files.FullName | ForEach-Object -Process { Get-Content -Path $_ | ConvertFrom-Json } | ConvertTo-Json -Depth 5 | Out-File -Path "$env:OneDriveCommercial\Documents\SpeedTestResults\merged.json" -Encoding UTF8 -Append
+$Files = Get-ChildItem -Path "$env:OneDriveCommercial\Documents\SpeedTestResults\" -Filter "Result-*.json"
+$Path = "$env:OneDriveCommercial\Documents\SpeedTestResults\Merged-" + ([datetime]::Today).ToShortDateString().replace("/", "-") + ".json"
+$Files.FullName | ForEach-Object -Process { Get-Content -Path $_ | ConvertFrom-Json } | ConvertTo-Json -Depth 5 | Out-File -Path $Path -Encoding UTF8 -Append
