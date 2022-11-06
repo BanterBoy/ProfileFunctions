dcfc7c979b45cc35c60222b75d979116b8bfb162
Luke Leigh <luke.leigh@gmail.com>
2022-11-06 23:39:50 +0000

    Update CredParamsInScript.ps1

--- CredParamsInScript.ps1
+++ CredParamsInScript.ps1
@@ -1,33 +1,32 @@
 [CmdletBinding(DefaultParameterSetName = 'Default',
-PositionalBinding = $true,
-SupportsShouldProcess = $true)]
+	PositionalBinding = $true,
+	SupportsShouldProcess = $true)]
 [OutputType([string], ParameterSetName = 'Default')]
-[Alias('cpsxc')]
+[Alias('something')]
 Param
 (
-[Parameter(ParameterSetName = 'Default',
-	Mandatory = $true,
-	ValueFromPipeline = $true,
-	ValueFromPipelineByPropertyName = $true,
-	ValueFromRemainingArguments = $true,
-	Position = 0,
-	HelpMessage = 'Enter the Name of the computer you would like to connect to.')]
-[Alias('cn')]
-[string[]]
-$ComputerName = $env:COMPUTERNAME,
-[Parameter(ParameterSetName = 'Default',
-	Mandatory = $false,
-	ValueFromPipeline = $true,
-	ValueFromPipelineByPropertyName = $true,
-	HelpMessage = 'Enter computer name or pipe input'
-)]
-[Alias('cred')]
-[ValidateNotNull()]
-[System.Management.Automation.PSCredential]
-[System.Management.Automation.Credential()]
-$Credential
+	[Parameter(ParameterSetName = 'Default',
+		Mandatory = $true,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true,
+		ValueFromRemainingArguments = $true,
+		Position = 0,
+		HelpMessage = 'Enter the Name of the computer you would like to connect to.')]
+	[Alias('cn')]
+	[string[]]
+	$ComputerName = $env:COMPUTERNAME,
+
+	[Parameter(ParameterSetName = 'Default',
+		Mandatory = $false,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true,
+		HelpMessage = 'Enter computer name or pipe input'
+	)]
+	[Alias('cred')]
+	[ValidateNotNull()]
+	[System.Management.Automation.PSCredential]
+	[System.Management.Automation.Credential()]
+	$Credential
 
 )
-
-
 
