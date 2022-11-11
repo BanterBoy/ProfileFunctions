5334513a1172335ff931d5bbdc0c583e11dc8757
Luke Leigh <luke.leigh@gmail.com>
2022-11-11 15:58:08 +0000

    Update Get-ProfileFunctions.ps1

--- ProfileFunctions/Get-ProfileFunctions.ps1
+++ ProfileFunctions/Get-ProfileFunctions.ps1
@@ -1,17 +1,8 @@
 function Get-ProfileFunctions {
-	$psPath = "C:\GitRepos\ProfileFunctions"
-	$funcs = @();
-	Get-ChildItem "$psPath\ProfileFunctions\*.ps1" |
-	ForEach-Object {
-		$funcs = $funcs + (Get-ScriptFunctionNames -Path "$psPath\ProfileFunctions\$($_.Name)")
+	function Get-Functions {
+		$psPath = "C:\GitRepos\ProfileFunctions"
+		$funcs = @();
+		Get-ChildItem "$psPath\ProfileFunctions\*.ps1" | Select-Object -Property BaseName
 	}
-	$funcs = $funcs + (Get-ScriptFunctionNames -Path "$psPath\Microsoft.PowerShell_profile.ps1")
-	Invoke-BatchArray -Arr ($funcs | Sort-Object) -BatchSize 4 | 
-	ForEach-Object {
-		$line = ''
-		$_ | ForEach-Object {
-			$line += $_.PadRight(40, ' ')
-		}
-		Write-Host($line)
-	}
+	Get-Functions | Format-Wide -Autosize
 }
