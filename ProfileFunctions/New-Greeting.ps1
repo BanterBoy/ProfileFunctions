50d5d3715097c29c7f016ed89a20d8d5bbe02853
Luke Leigh <luke.leigh@gmail.com>
2023-02-27 02:47:53 +0000

    Update New-Greeting.ps1

--- ProfileFunctions/New-Greeting.ps1
+++ ProfileFunctions/New-Greeting.ps1
@@ -1,11 +1,71 @@
 function New-Greeting {
-	$Today = $(Get-Date)
-	Write-Host "Day of Week -"$Today.DayOfWeek " - Today's Date -"$Today.ToShortDateString() "- Current Time -"$Today.ToShortTimeString()
-	Switch ($Today.dayofweek) {
-		Monday { Write-Host "Don't want to work today" }
-		Friday { Write-Host "Almost the weekend" }
-		Saturday { Write-Host "Everyone loves a Saturday ;-)" }
-		Sunday { Write-Host "A good day to rest, or so I hear." }
-		Default { Write-Host "Business as usual." }
+	[CmdletBinding(DefaultParameterSetName = 'Default')]
+	[OutputType([string])]
+	Param
+	(
+		[Parameter(ParameterSetName = 'Default',
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ValueFromPipelineByPropertyName = $true,
+			HelpMessage = 'Enter '
+		)]
+        [ValidateSet([GreetingsValidator])]
+		[string]$Day
+	)
+
+	Begin {
+        $greetings = [Greetings]::new()
+
+		if ([String]::IsNullOrWhiteSpace($Day)) {
+			$Day = $greetings.CurrentDay
+		}
 	}
+
+	Process {
+		Write-Output $($greetings.GetMessageForDay($Day))
+	}
+}
+
+class Greetings {
+    $greetings = @{
+        Monday		= "Don't want to work today"
+        Friday  	= "Almost the weekend"
+        Saturday 	= "Everyone loves a Saturday ;-)"
+        Sunday 		= "A good day to rest, or so I hear."
+    }
+
+    [string[]] $Days = @( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" )
+
+    $Date = $(Get-Date)
+
+    [string] $CurrentDay = $this.Date.DayOfWeek
+
+    [object] GetMessageForDay([string] $day) {
+        $time = $this.Date.ToShortTimeString()
+
+        $message = $this.greetings[$day]
+
+        if ([String]::IsNullOrWhiteSpace($message)) {
+            $message = "Business as usual."
+        }
+
+        $properties = [ordered]@{
+            "Date"    = $this.Date.ToShortDateString()
+            "Day"     = $day
+            "Time"    = $time
+            "Message" = $message
+        }
+        
+        return New-Object -TypeName PSObject -Property $properties
+    }
+
+    Process([String] $name) {
+        Write-Host($this.asciiMappings[$name] | Set-Clipboard -PassThru)
+    }
+}
+
+class GreetingsValidator : System.Management.Automation.IValidateSetValuesGenerator {
+    [String[]] GetValidValues() {
+        return [Greetings]::new().Days
+    }
 }
