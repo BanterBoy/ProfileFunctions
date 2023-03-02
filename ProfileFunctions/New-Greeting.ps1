98f9e9f15d266e2ef2664119040b7a633a8dc479
Luke Leigh <luke.leigh@gmail.com>
2023-03-02 21:24:20 +0000

    Update New-Greeting.ps1

--- ProfileFunctions/New-Greeting.ps1
+++ ProfileFunctions/New-Greeting.ps1
@@ -1,43 +1,42 @@
 function New-Greeting {
-	[CmdletBinding(DefaultParameterSetName = 'Default')]
-	[OutputType([string])]
-	Param
-	(
-		[Parameter(ParameterSetName = 'Default',
-			Mandatory = $false,
-			ValueFromPipeline = $true,
-			ValueFromPipelineByPropertyName = $true,
-			HelpMessage = 'Enter '
-		)]
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(ParameterSetName = 'Default',
+            Mandatory = $false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Enter '
+        )]
         [ValidateSet([GreetingsValidator])]
-		[string]$Day
-	)
+        [string]$Day
+    )
 
-	Begin {
+    Begin {
         $greetings = [Greetings]::new()
 
-		if ([String]::IsNullOrWhiteSpace($Day)) {
-			$Day = $greetings.CurrentDay
-		}
-	}
+        if ([String]::IsNullOrWhiteSpace($Day)) {
+            $Day = $greetings.CurrentDay
+        }
+    }
 
-	Process {
-		Write-Output $($greetings.GetMessageForDay($Day))
-	}
+    Process {
+        Write-Output $($greetings.GetMessageForDay($Day))
+    }
 }
 
 class Greetings {
-    $greetings = @{
-        Monday		= "Don't want to work today"
-        Friday  	= "Almost the weekend"
-        Saturday 	= "Everyone loves a Saturday ;-)"
-        Sunday 		= "A good day to rest, or so I hear."
-    }
 
+    $greetings = @{
+        Monday   = "Don't want to work today"
+        Friday   = "Almost the weekend"
+        Saturday = "Everyone loves a Saturday ;-)"
+        Sunday   = "A good day to rest, or so I hear."
+    }
     [string[]] $Days = @( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" )
 
     $Date = $(Get-Date)
-
     [string] $CurrentDay = $this.Date.DayOfWeek
 
     [object] GetMessageForDay([string] $day) {
