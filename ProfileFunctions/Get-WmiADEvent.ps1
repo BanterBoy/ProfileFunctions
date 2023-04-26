c631da1db7391d032817947a3a42f66733eefccb
Luke Leigh <luke.leigh@gmail.com>
2023-04-26 20:17:27 +0100

    Update Get-WmiADEvent.ps1

--- ProfileFunctions/Get-WmiADEvent.ps1
+++ ProfileFunctions/Get-WmiADEvent.ps1
@@ -1,23 +1,22 @@
 Function Get-WmiADEvent {
     Param([string]$query)
   
-    $Path="root\directory\ldap"
-    $EventQuery  = New-Object System.Management.WQLEventQuery $query
-    $Scope       = New-Object System.Management.ManagementScope $Path
-    $Watcher     = New-Object System.Management.ManagementEventWatcher $Scope,$EventQuery
-    $Options     = New-Object System.Management.EventWatcherOptions
+    $Path = "root\directory\ldap"
+    $EventQuery = New-Object System.Management.WQLEventQuery $query
+    $Scope = New-Object System.Management.ManagementScope $Path
+    $Watcher = New-Object System.Management.ManagementEventWatcher $Scope, $EventQuery
+    $Options = New-Object System.Management.EventWatcherOptions
     $Options.TimeOut = [timespan]"0.0:0:1"
     $Watcher.Options = $Options
-    cls
-    Write-Host ("Waiting for events in response to: {0}" -F $EventQuery.querystring)  -backgroundcolor cyan -foregroundcolor black
+    Write-Output "("Waiting for events in response to: { 0 }" -F $($EventQuery.querystring))"
     $Watcher.Start()
     while ($true) {
-       trap [System.Management.ManagementException] {continue}
+        trap [System.Management.ManagementException] { continue }
   
-       $Evt=$Watcher.WaitForNextEvent()
+        $Evt = $Watcher.WaitForNextEvent()
         if ($Evt) {
-           $Evt.TargetInstance | select *
-        Clear-Variable evt
+            $Evt.TargetInstance | Select-Object *
+            Clear-Variable evt
         }
     }
 }
