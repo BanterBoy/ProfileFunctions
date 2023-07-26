89640413385d8d6ce2cc0191ac6612898c8e8462
Luke Leigh <luke.leigh@gmail.com>
2023-07-27 00:20:52 +0100

    Update Get-ProfileFunctions.ps1

--- ProfileFunctions/Get-ProfileFunctions.ps1
+++ ProfileFunctions/Get-ProfileFunctions.ps1
@@ -1,8 +1,7 @@
 function Get-ProfileFunctions {
 	function Get-Functions {
-		$psPath = "C:\GitRepos\ProfileFunctions"
 		$funcs = @();
-		Get-ChildItem "$psPath\ProfileFunctions\*.ps1" | Select-Object -Property BaseName
+		Get-ChildItem "$PSScriptRoot\*.ps1" | Select-Object -Property BaseName
 	}
 	Get-Functions | Format-Wide -Autosize
 }
