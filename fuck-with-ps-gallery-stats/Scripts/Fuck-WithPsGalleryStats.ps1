d4607db99dc4fce1a2b3e0ab1e26b340e08ec1fc
Luke Leigh <luke.leigh@gmail.com>
2023-09-05 14:09:16 +0100

    Create Fuck-WithPsGalleryStats.ps1

---
+++ fuck-with-ps-gallery-stats/Scripts/Fuck-WithPsGalleryStats.ps1
@@ -0,0 +1,5 @@
+. $PSScriptRoot\..\ProfileFunctions\Fuck-WithPsGalleryStats.ps1
+
+# Fuck-WithPsGalleryStats -DownloadCount $Random -ModuleNames @('Transmission', 'JsonToPowershellClass', 'Flatten-Folders', 'ImageDataUriConverter', 'VideoResolution', 'GithubRepoSnapshot') -IntervalSeconds 9 -MultipleInstallsPerSession
+
+Fuck-WithPsGalleryStats -ModuleNames @('PSCiscoMeraki', 'PSCovid19Stats', 'PSRandomUser', 'Test-DNSRecord', 'PSPasswordWolf', 'PSIISCrypto') -DownloadCount 7 -IntervalSeconds 13 -MultipleInstallsPerSession
