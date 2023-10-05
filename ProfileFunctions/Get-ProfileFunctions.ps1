911eacde35171dbc4a62354fc5906f16e2a16160
Luke Leigh <luke.leigh@gmail.com>
2023-10-05 22:55:22 +0100

    Update Get-ProfileFunctions.ps1

--- ProfileFunctions/Get-ProfileFunctions.ps1
+++ ProfileFunctions/Get-ProfileFunctions.ps1
@@ -1,7 +1,23 @@
 function Get-ProfileFunctions {
-	function Get-Functions {
+
+	function Get-FunctionsPS7 {
 		$funcs = @();
 		Get-ChildItem "$PSScriptRoot\*.ps1" | Select-Object -Property BaseName
+		Get-ChildItem "$PSScriptRoot\7Only\*.ps1" | Select-Object -Property BaseName
 	}
-	Get-Functions | Format-Wide -Autosize
+
+	function Get-FunctionsPSEarlier {
+		$funcs = @();
+		Get-ChildItem "$PSScriptRoot\*.ps1" | Select-Object -Property BaseName
+		Get-ChildItem "$PSScriptRoot\6Instead\*.ps1" | Select-Object -Property BaseName
+	}
+
+	if ($PSVersionTable.PSVersion.Major -ge 6) {
+		Get-FunctionsPS7 | Format-Wide -Autosize
+	}
+	else {
+		Get-FunctionsPSEarlier | Format-Wide -Autosize
+	}
+
+
 }
