8218477e2b564595e29a9ace6c35febd342b5691
Luke Leigh <luke.leigh@gmail.com>
2023-10-19 23:45:29 +0100

    Update Get-WTFismyIP.ps1

--- ProfileFunctions/Get-WTFismyIP.ps1
+++ ProfileFunctions/Get-WTFismyIP.ps1
@@ -1,8 +1,33 @@
+<#
+.SYNOPSIS
+    Retrieves IP address information from wtfismyip.com.
+
+.DESCRIPTION
+    The Get-WTFismyIP function retrieves IP address information from wtfismyip.com and returns it as a PowerShell object.
+
+.PARAMETER Polite
+    If specified, the function will not include profanity in the output.
+
+.EXAMPLE
+    Get-WTFismyIP
+
+    Retrieves IP address information from wtfismyip.com and returns it as a PowerShell object.
+
+.EXAMPLE
+    Get-WTFismyIP -Polite
+
+    Retrieves IP address information from wtfismyip.com and returns it as a PowerShell object, without including profanity in the output.
+
+.NOTES
+    Author: Unknown
+    Last Edit: Unknown
+#>
+
 function Get-WTFismyIP {
     [CmdletBinding()]
 
     param (
-        [switch] $Polite        
+        [switch] $Polite
     )
     
     begin { }
