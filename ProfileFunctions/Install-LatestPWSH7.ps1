05c085f89efd3b712a47960dd0aeee54ea0122d7
Luke Leigh <luke.leigh@gmail.com>
2023-10-21 01:42:36 +0100

    Update Install-LatestPWSH7.ps1

--- ProfileFunctions/Install-LatestPWSH7.ps1
+++ ProfileFunctions/Install-LatestPWSH7.ps1
@@ -1,3 +1,25 @@
 function Install-LatestPWSH7 {
-    Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
+    # Check for existing installation
+    if (Get-Command pwsh -ErrorAction SilentlyContinue) {
+        $installedVersion = (pwsh --version).Split(" ")[-1]
+        $latestVersion = (Invoke-RestMethod https://api.github.com/repos/PowerShell/PowerShell/releases/latest).tag_name.Replace("v", "")
+        
+        if ($installedVersion -eq $latestVersion) {
+            Write-Output "The latest version of PowerShell 7 ($latestVersion) is already installed."
+            return
+        }
+        else {
+            Write-Output "An older version of PowerShell 7 ($installedVersion) is installed. The latest version is $latestVersion."
+        }
+    }
+
+    # Attempt to install PowerShell 7
+    try {
+        Write-Verbose "Attempting to install PowerShell 7..."
+        Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
+        Write-Verbose "PowerShell 7 installed successfully."
+    }
+    catch {
+        Write-Error "Failed to install PowerShell 7: $_"
+    }
 }
