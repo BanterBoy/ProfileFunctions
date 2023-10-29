dbd7866a7736433c9029ea53dec4553f85bfa059
Luke Leigh <luke.leigh@gmail.com>
2023-10-29 16:31:40 +0000

    Update Install-PSTools.ps1

--- ProfileFunctions/Install-PSTools.ps1
+++ ProfileFunctions/Install-PSTools.ps1
@@ -1,7 +1,47 @@
 function Install-PSTools {
-	Invoke-WebRequest -Uri 'https://download.sysinternals.com/files/PSTools.zip' -OutFile 'pstools.zip'
-	Expand-Archive -Path 'pstools.zip' -DestinationPath "$env:TEMP\pstools"
-	New-Item -Path 'C:\Program Files\Sysinternals'
-	Copy-Item -Path "$env:TEMP\pstools\*.*" -Destination 'C:\Program Files\Sysinternals'
-	Remove-Item -Path "$env:TEMP\pstools" -Recurse
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	param()
+
+	if ($PSCmdlet.ShouldProcess('PSTools', 'Install')) {
+		Write-Verbose "Checking if PSTools is already installed..."
+		if (Test-Path 'C:\Program Files\Sysinternals\PsExec.exe') {
+			Write-Verbose "PSTools is already installed."
+			return
+		}
+
+		try {
+			Write-Verbose "Downloading PSTools..."
+			Invoke-WebRequest -Uri 'https://download.sysinternals.com/files/PSTools.zip' -OutFile 'pstools.zip'
+
+			Write-Verbose "Extracting PSTools..."
+			Expand-Archive -Path 'pstools.zip' -DestinationPath "$env:TEMP\pstools"
+
+			Write-Verbose "Checking if 'C:\Program Files\Sysinternals' directory exists..."
+			if (!(Test-Path 'C:\Program Files\Sysinternals')) {
+				Write-Verbose "Creating 'C:\Program Files\Sysinternals' directory..."
+				New-Item -Path 'C:\Program Files\Sysinternals'
+			}
+
+			Write-Verbose "Copying PSTools to 'C:\Program Files\Sysinternals'..."
+			Copy-Item -Path "$env:TEMP\pstools\*.*" -Destination 'C:\Program Files\Sysinternals'
+
+			Write-Verbose "Cleaning up temporary files..."
+			Remove-Item -Path "$env:TEMP\pstools" -Recurse
+			Remove-Item -Path 'pstools.zip'
+
+			Write-Verbose "Checking if user is an administrator..."
+			$test = Test-IsAdmin
+			if ($test -eq $true) {
+				Write-Verbose "User is an administrator. Adding 'C:\Program Files\Sysinternals' to system 'Path'..."
+				Add-EnvPath -Path 'C:\Program Files\Sysinternals' -Container 'Machine'
+			}
+			else {
+				Write-Verbose "User is not an administrator. Adding 'C:\Program Files\Sysinternals' to user 'Path'..."
+				Add-EnvPath -Path 'C:\Program Files\Sysinternals' -Container 'User'
+			}
+		}
+		catch {
+			Write-Error "An error occurred: $_"
+		}
+	}
 }
