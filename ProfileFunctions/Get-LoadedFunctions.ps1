df7eecfd724090adf3918249376a1e6e149ec9b8
Luke Leigh <luke.leigh@gmail.com>
2023-10-30 00:13:08 +0000

    Update Get-LoadedFunctions.ps1

--- ProfileFunctions/Get-LoadedFunctions.ps1
+++ ProfileFunctions/Get-LoadedFunctions.ps1
@@ -1,4 +1,30 @@
 function Get-LoadedFunctions {
+    <#
+    .SYNOPSIS
+        Gets a list of loaded functions based on the specified verb and noun.
+    
+    .DESCRIPTION
+        This function retrieves a list of loaded functions based on the specified verb and noun. If no verb or noun is specified, all loaded functions are returned.
+    
+    .PARAMETER Verb
+        Specifies the verb to filter the loaded functions by. Wildcards are supported.
+    
+    .PARAMETER Noun
+        Specifies the noun to filter the loaded functions by. Wildcards are supported.
+    
+    .PARAMETER Wide
+        If specified, the output is displayed in a wide format.
+    
+    .EXAMPLE
+        Get-LoadedFunctions -Verb Get -Noun Item
+    
+        This command retrieves a list of loaded functions that have "Get" as the verb and "Item" as the noun.
+    
+    .EXAMPLE
+        Get-LoadedFunctions -Wide
+    
+        This command retrieves a list of all loaded functions and displays the output in a wide format.
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
