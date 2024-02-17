9be55852fce89b5b23df47eb36c1890fd2a5b96a
Luke Leigh <luke.leigh@gmail.com>
2024-02-17 17:10:41 +0000

    Update Set-TransmissionDefaultSettings.ps1

--- ProfileFunctions/Set-TransmissionDefaultSettings.ps1
+++ ProfileFunctions/Set-TransmissionDefaultSettings.ps1
@@ -1,3 +1,5 @@
+
+
 function Set-TransmissionDefaultSettings {
 
     <#
@@ -11,7 +13,7 @@ function Set-TransmissionDefaultSettings {
             AlternativeSpeedTimeEnd     = 60
             AlternativeSpeedUp          = 256
             BlockListEnabled            = $true
-            BlockListUrl                = "https=//github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz"
+            BlockListUrl                = "https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz"
             CacheSizeMb                 = 8
             DhtEnabled                  = $true
             DownloadDirectory           = "/share/Public"
@@ -51,48 +53,48 @@ function Set-TransmissionDefaultSettings {
     #>
 
     $properties = @{
-		AlternativeSpeedDown        = 51200
-		AlternativeSpeedEnabled     = $true
-		AlternativeSpeedTimeBegin   = 60
-		AlternativeSpeedTimeEnabled = $true
-		AlternativeSpeedTimeEnd     = 480
-		AlternativeSpeedTimeDay     = 127
-		AlternativeSpeedUp          = 1024
-		BlockListEnabled            = $true
-		BlockListUrl                = "https=//github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz"
-		CacheSizeMb                 = 256
-		DownloadDirectory           = "/share/Public"
-		DownloadQueueSize           = 1
-		DownloadQueueEnabled        = $true
-		DhtEnabled                  = $true
-		Encryption                  = "required"
-		IdleSeedingLimit            = 0
-		IdleSeedingLimitEnabled     = $true
-		IncompleteDirectory         = "/share/Public"
-		IncompleteDirectoryEnabled  = $true
-		LpdEnabled                  = $false
-		PeerLimitGlobal             = 500
-		PeerLimitPerTorrent         = 250
-		PeerPort                    = 51413
-		PeerPortRandomOnStart       = $false
-		PexEnabled                  = $true
-		PortForwardingEnabled       = $true
-		QueueStalledEnabled         = $true
-		QueueStalledMinutes         = 0
-		RenamePartialFiles          = $true
-		ScriptTorrentDoneFilename   = $false
-		ScriptTorrentDoneEnabled    = $false
-		SeedRatioLimit              = 0
-		SeedRatioLimited            = $true
-		SeedQueueSize               = 0
-		SeedQueueEnabled            = $true
-		SpeedLimitDown              = 1024
-		SpeedLimitDownEnabled       = $true
-		SpeedLimitUp                = 512
-		SpeedLimitUpEnabled         = $true
-		StartAddedTorrents          = $true
-		TrashOriginalTorrentFiles   = $false
-		UtpEnabled                  = $true
+        AlternativeSpeedDown        = 51200
+        AlternativeSpeedEnabled     = $true
+        AlternativeSpeedTimeBegin   = 60
+        AlternativeSpeedTimeEnabled = $true
+        AlternativeSpeedTimeEnd     = 480
+        AlternativeSpeedTimeDay     = 127
+        AlternativeSpeedUp          = 1024
+        BlockListEnabled            = $true
+        BlockListUrl                = "https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz"
+        CacheSizeMb                 = 256
+        DownloadDirectory           = "/share/Public"
+        DownloadQueueSize           = 1
+        DownloadQueueEnabled        = $true
+        DhtEnabled                  = $true
+        Encryption                  = "required"
+        IdleSeedingLimit            = 0
+        IdleSeedingLimitEnabled     = $true
+        IncompleteDirectory         = "/share/Public"
+        IncompleteDirectoryEnabled  = $true
+        LpdEnabled                  = $false
+        PeerLimitGlobal             = 500
+        PeerLimitPerTorrent         = 250
+        PeerPort                    = 51413
+        PeerPortRandomOnStart       = $false
+        PexEnabled                  = $true
+        PortForwardingEnabled       = $true
+        QueueStalledEnabled         = $true
+        QueueStalledMinutes         = 0
+        RenamePartialFiles          = $true
+        ScriptTorrentDoneFilename   = $false
+        ScriptTorrentDoneEnabled    = $false
+        SeedRatioLimit              = 0
+        SeedRatioLimited            = $true
+        SeedQueueSize               = 0
+        SeedQueueEnabled            = $true
+        SpeedLimitDown              = 1024
+        SpeedLimitDownEnabled       = $true
+        SpeedLimitUp                = 512
+        SpeedLimitUpEnabled         = $true
+        StartAddedTorrents          = $true
+        TrashOriginalTorrentFiles   = $false
+        UtpEnabled                  = $true
     }
     
     Set-TransmissionSession @properties
