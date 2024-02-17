57ebacdf553fed133d394d507664a69c696ccf1c
Luke Leigh <luke.leigh@gmail.com>
2024-02-17 17:10:45 +0000

    Update New-Greeting.ps1

--- ProfileFunctions/7Only/New-Greeting.ps1
+++ ProfileFunctions/7Only/New-Greeting.ps1
@@ -25,3 +25,46 @@ function New-Greeting {
         Write-Output $($greetings.GetMessageForDay($Day))
     }
 }
+
+class Greetings {
+
+    $greetings = @{
+        Monday   = "Don't want to work today"
+        Friday   = "Almost the weekend"
+        Saturday = "Everyone loves a Saturday ;-)"
+        Sunday   = "A good day to rest, or so I hear."
+    }
+    [string[]] $Days = @( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" )
+
+    $Date = $(Get-Date)
+    [string] $CurrentDay = $this.Date.DayOfWeek
+
+    [object] GetMessageForDay([string] $day) {
+        $time = $this.Date.ToShortTimeString()
+
+        $message = $this.greetings[$day]
+
+        if ([String]::IsNullOrWhiteSpace($message)) {
+            $message = "Business as usual."
+        }
+
+        $properties = [ordered]@{
+            "Date"    = $this.Date.ToShortDateString()
+            "Day"     = $day
+            "Time"    = $time
+            "Message" = $message
+        }
+        
+        return New-Object -TypeName PSObject -Property $properties
+    }
+
+    Process([String] $name) {
+        Write-Host($this.asciiMappings[$name] | Set-Clipboard -PassThru)
+    }
+}
+
+class GreetingsValidator : System.Management.Automation.IValidateSetValuesGenerator {
+    [String[]] GetValidValues() {
+        return [Greetings]::new().Days
+    }
+}
